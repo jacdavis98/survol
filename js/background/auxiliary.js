a03c30d7572c0f4829f9db93928d3a18c0e8b48a
jacdavis98 <masterperson141@live.com>
2023-04-08 20:51:43 -0400

    updated to support manifest v3 and refactored for modularity and efficiency

--- js/background/auxiliary.js
+++ js/background/auxiliary.js
@@ -1,66 +1,57 @@
-var REQUEST_CACHE = {};
+const REQUEST_CACHE = {};
 
 // Clear cache every 10 mins
 setInterval(() => {
-    REQUEST_CACHE = {};
+    Object.keys(REQUEST_CACHE).length = 0;
 }, 1000 * 60 * 10);
 
+const handleStateRequest = (req, sender) => {
+    chrome.action.setBadgeText({
+        text: req.data,
+        tabId: sender.tab.id
+    });
+};
 
-/* <https://www.chromium.org/Home/chromium-security/extension-content-script-fetches>
- * Later in 2019, Extension Manifest V3 will become available, requiring cross-origin requests to occur in background pages rather than content scripts.  This new manifest version will have its own migration period, before support for Extension Manifest V2 is eventually removed from Chrome.
- * Using the background script to pull data from APIs safely
- */
-chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
-    if (req.action == 'state') {
-        var state = req.data;
-        chrome.browserAction.setBadgeText({
-            text: state,
-            tabId: sender.tab.id
-        });
-    }
-    if (req.action == 'request') {
+const handleFetchRequest = async (req, sendResponse) => {
+    let res = { status: 'error', data: null };
 
-        let res = { status: 'error', data: null };
+    if (REQUEST_CACHE[req.data.url]) {
+        res = REQUEST_CACHE[req.data.url];
+        res.cached = true;
+        sendResponse(res);
+    } else {
+        try {
+            const data = await fetch(req.data.url);
+            const result = req.data.noJSON ? await data.text() : await data.json();
 
-        // if the request is cached
-        if (REQUEST_CACHE[req.data.url]) {
-            res = REQUEST_CACHE[req.data.url];
-            res.cached = true;
+            res.data = result;
+            res.status = 'OK';
+            res.cached = false;
+            REQUEST_CACHE[req.data.url] = res;
             sendResponse(res);
-        }
-
-        // If the request isn't cached
-        else {
-            fetch(req.data.url)
-                .then((data) => { return req.data.noJSON ? data.text() : data.json(); })
-                .then((data) => {
-                    res.data = data;
-                    res.status = 'OK';
-                    res.cached = false;
-                    REQUEST_CACHE[req.data.url] = res;
-                    sendResponse(res);
-                })
-                .catch((error) => {
-                    res.data = error;
-                    res.status = 'error';
-                    sendResponse(res);
-                    console.error('SURVOL - Fetching error', error);
-                });
+        } catch (error) {
+            res.data = error;
+            res.status = 'error';
+            sendResponse(res);
+            console.error('SURVOL - Fetching error', error);
         }
     }
+};
 
-    // To avoid leaving the message hanging
-    else {
+chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
+    if (req.action === 'state') {
+        handleStateRequest(req, sender);
+    } else if (req.action === 'request') {
+        handleFetchRequest(req, sendResponse);
+    } else {
         sendResponse({ action: 'none', status: 'OK' });
     }
 
     return true;
 });
 
-
-// Those are the extension default settings
 const DEFAULT_SETTINGS = {
-    version: '0.6.0',
+    version: '0.7.0',
     disabledDomains: ['survol.me'],
     selfReferDisabled: ['github.com', 'ppy.sh'],
     previewMetadata: true,
@@ -68,30 +59,21 @@ const DEFAULT_SETTINGS = {
     installationType: 'install'
 };
 
-// When the extension is installed
 chrome.runtime.onInstalled.addListener(() => {
-
-    // Initialize settings
     chrome.storage.local.get(Object.keys(DEFAULT_SETTINGS), (res) => {
-
-        // In order to chose between "Thanks for installing the extension" and "Survol has been updated"
         let oldVersion = res.version;
-
         res.version = DEFAULT_SETTINGS.version;
 
         if (res.installationType) {
-            res.installationType = (res.version == oldVersion) ? 'none' : 'update';
+            res.installationType = (res.version === oldVersion) ? 'none' : 'update';
         }
 
         Object.keys(DEFAULT_SETTINGS).forEach((key) => {
             res[key] = res[key] || DEFAULT_SETTINGS[key];
         });
 
-        // Save settings then open onboarding page
         chrome.storage.local.set(res, () => {
-
-            // If there has been an update or the extension has just been installed
-            if (res.installationType == 'update' || res.installationType == 'install') {
+            if (res.installationType === 'update' || res.installationType === 'install') {
                 chrome.tabs.create({ url: chrome.runtime.getURL('./html/onboarding.html') });
             }
         });
